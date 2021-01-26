845097b12f1f13a7527f1d4ce56a34fa51d1fe6d
Rafael Tavares <rafael.tawares@gmail.com>
2021-01-26 18:23:56 -0300

    CSS settings

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -3,8 +3,7 @@ import styled from 'styled-components'
 const QuizContainer = styled.div`
   width: 100%;
   max-width: 400px;
-  padding-top: 40px;
-  margin: auto;
+  margin: 40px auto;
 
   @media screen and (max-width: 500px) {
     padding: 15px;
